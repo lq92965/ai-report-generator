46055f3d0ef51a0d0a60be7f6d58343755530d4c
lq92965 <lq92965@gmail.com>
2025-12-30 15:40:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    const API_BASE_URL = 'http://68.183.162.193:3000'; 
+    const API_BASE_URL = 'https://api.goreportify.com'; 
     let allTemplates = []; 
     let currentUserPlan = 'basic'; 
 
@@ -791,6 +791,3 @@ if (payButtons.length > 0) {
 
 }); 
 // End of Script
-
-
-
